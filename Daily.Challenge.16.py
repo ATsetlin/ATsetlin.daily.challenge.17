af3d0ab46a24266664c8062415935a70889fdc53
Aryeh (Ari) Tsetlin <57076256+ATsetlin@users.noreply.github.com>
2019-11-18 22:39:12 +0200

    Delete Daily.Challenge.16.py

--- Daily.Challenge.16.py
+++
@@ -1,1 +0,0 @@
-!DOCTYPE.html <html><head><title></title></head> <body> <h1> Bucky Barnes</h1> <p> This is the last time this will be so easy. I am 30 yrs old and I am the Winter Soldier. I was born in Brooklyn and became a Nazi Secret Weapon during WWII and was forced to fight my best friend in Manhattan in 2015</p> </body> </html>
